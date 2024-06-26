8ffab6770737032f6e2f993f5b0e097b6d08198a
David Hérault <dherault@gmail.com>
2024-06-26 13:21:47 +0300

    v1.3.1

--- dist/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-export declare type DifferenceType = ['inserted' | 'deleted' | 'modified', number, any?];
-declare function arrayDifferences(arrayA: any[], arrayB: any[], comparisonFn?: typeof defaultComparisonFn): DifferenceType[];
-declare namespace arrayDifferences {
-    var reconstructArray: (array: any[], differences: DifferenceType[], inPlace?: boolean) => any[];
-}
-declare function defaultComparisonFn(x: any, y: any): boolean;
-export default arrayDifferences;
